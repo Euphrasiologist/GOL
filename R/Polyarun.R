4e65beb80c0829fa00db7134e557c79cf2f62311
Max Brown <max.brown@ed.ac.uk>
2019-06-15 18:52:28 +0100

    First upload. More to follow!

---
+++ R/Polyarun.R
@@ -0,0 +1,40 @@
+#' Polyaurn function
+#'
+#' Another implementation of the urn game from Games of Life by Karl Sigmund (ISBN: 978-0198547839)
+#' This time, an example of runaway drift with an unlikely twist.
+#' @params Red, number of red balls. Black, number of black balls. Trials, how many trials?
+#' @keywords polyaurn, gamesoflife, genetics
+#' @export
+#' @examples
+#' urn(1,1,1000)
+
+polyaurn <- function(red, black, trials){
+  red <- red
+  black <- black
+
+  dat <- data.frame(trial = 1:trials,
+                    colour = vector(length = trials))
+
+  for(i in 1:trials){
+    if(sample(x = c(rep("red", red), rep("black", black)), size = 1, replace = T, prob = rep(1/(red+black), sum(red, black))) == "red"){
+      red <- red + 1
+      dat$colour[i] <- sample(x = c(rep("red", red), rep("black", black)), size = 1, replace = T, prob = rep(1/(red+black), sum(red, black)))
+    } else
+      black <- black + 1
+    dat$colour[i] <- sample(x = c(rep("red", red), rep("black", black)), size = 1, replace = T, prob = rep(1/(red+black), sum(red, black)))
+  }
+
+  dat$outcome <- as.numeric(dat$colour == "red")
+  setDT(dat)
+  dat <- dat[, cum.sum := cumsum(outcome)/trial]
+
+
+  return(list(ggplot(dat, aes(x = trial, y = cum.sum))+
+                geom_hline(yintercept = 0.5, col = "red", size=0.3)+
+                geom_line()+
+                xlab(label = "Trial Number")+
+                ylab(label = "P(red)")+
+                theme_bw(base_line_size = 0),
+              dat,
+              table(dat$colour)))
+}
---
+++ R/Urn.R
@@ -0,0 +1,41 @@
+
+#' Urn function
+#'
+#' Implementation of the urn game from Games of Life by Karl Sigmund (ISBN: 978-0198547839)
+#' @params Red, number of red balls. Black, number of black balls. Trials, how many trials?
+#' @keywords urn, gamesoflife, genetics
+#' @export
+#' @examples
+#' urn(1,1,1000)
+
+library(data.table)
+library(ggplot2)
+
+urn <- function(red, black, trials){
+  red <- red
+  black <- black
+
+  dat <- data.frame(trial = 1:trials,
+                    colour = vector(length = trials))
+  for(i in 1:trials){
+    dat$colour[i] <- sample(x = c(rep("red", red), rep("black", black)), size = 1, replace = T, prob = rep(1/(red+black), sum(red, black)) )
+  }
+
+  dat$outcome <- as.numeric(dat$colour == "red")
+  setDT(dat)
+  dat <- dat[, cum.sum := cumsum(outcome)/trial]
+
+
+  return(list(ggplot(dat, aes(x = trial, y = cum.sum))+
+                geom_hline(yintercept = 0.5, col = "red", size=0.3)+
+                geom_line()+
+                xlab(label = "Trial Number")+
+                ylab(label = "P(red)")+
+                theme_bw(base_line_size = 0),
+         dat))
+}
+
+
+
+
+
